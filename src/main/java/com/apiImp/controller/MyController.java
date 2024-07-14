4f594a04bfc8b723bf18bb67271b582150bfc43e
Ghanshyam <ghanshyam7595@gmail.com>
2024-07-15 03:54:47 +0530

    controoler update

--- src/main/java/com/apiImp/controller/MyController.java
+++ src/main/java/com/apiImp/controller/MyController.java
@@ -1,5 +1,18 @@
 package com.apiImp.controller;
 
-public class MyController {
+import jakarta.persistence.criteria.Order;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.Optional;
+
+public class MyController {
+    @GetMapping("/{id}")
+    public ResponseEntity<Order> getOrderById(@PathVariable Long id) {
+        MyController orderService = null;
+        ResponseEntity<Order> order;
+        order = orderService.getOrderById(id);
+        return order.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
